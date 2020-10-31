1dbf8e3c2a8f80bd60076765f677c130c6b47b62
salcedo12 <saluis7@misena.edu.co>
2020-10-31 11:49:04 -0500

    sección 7 de objectos terminada

--- 07-Objetos/js/12-app.js
+++ 07-Objetos/js/12-app.js
@@ -8,4 +8,13 @@ const producto = {
 
 }
 
-//object
+//object constructor
+
+function Producto (nombre, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = true;
+}
+
+const producto2 = new Producto('Monitor 24 ', 433);
+console.log(producto2);
---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,15 @@
+// object literal
+const producto = { 
+    nombre: "Monitor 20 Pulgadas",
+    precio: 400000,
+    disponible:true,
+   
+
+}
+
+//revisa las llaves o propiedades del objecto
+console.log(Object.keys(producto));
+// retorna los valores
+console.log(Object.values(producto));
+//retorna todo
+console.log(Object.entries(producto));
